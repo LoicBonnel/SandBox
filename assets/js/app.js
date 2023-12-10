f3cc588d09bfdab89b16c5d779128ed672a5e261
THLDiamond <bonnelloic62@gmail.com>
2023-12-10 11:08:30 +0100

    Add Playlist

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+// app.js
+
 console.log("Hello Saint Jo");
 
 const prenom = 'Loïc'; // string
@@ -9,19 +11,34 @@ const sac = {livre: 'BD Tintin', trousse: 'stylo', skills: ["Bagarre", "Jeux Vid
 const disque = document.querySelector(".disque");
 const btnPause = document.getElementById("btnPause");
 const paragraphe = document.querySelector(".div1 p");
+const playlist = document.getElementById("btnPlaylist");
+
 
 setTimeout(() => {
     paragraphe.style.backgroundColor = "blue";
 }, 4000);
 
-btnPause.addEventListener("click", ()=> {
+
+playlist.addEventListener("click", async (event) => {
+    event.preventDefault(); // Empêche le comportement par défaut du lien
+    console.log("Bouton Playlist cliqué");
+    const playlistContainer = document.querySelector('.listPlaylist');
+    playlistContainer.classList.add("visible"); // Ajoutez la classe visible
+});
+
+
+
+btnPause.addEventListener("click", async () => {
     disque.classList.toggle("pause");
 
-    if (disque.classList.contains("pause")){
+    if (disque.classList.contains("pause")) {
         btnPause.textContent = "Play";
-    }else{
+    } else {
         btnPause.textContent = "Pause";
     }
+
+    // Ajouter une petite pause pour laisser le temps au changement de classe d'être appliqué
+    await new Promise(resolve => setTimeout(resolve, 0));
 });
 
 console.log(disque);
@@ -29,14 +46,11 @@ console.log(disque);
 console.log(apprenants[2]);
 console.log(sac.skills[0]);
 
-apprenants.forEach(  // Boucle sur le tableau
-    prenom => {
-        console.log(`Bonjour ${prenom}`)
-    }
-)
+apprenants.forEach(prenom => {
+    console.log(`Bonjour ${prenom}`)
+});
 
-
-console.log(`Bonjour ${prenom}`); //concatenation
+console.log(`Bonjour ${prenom}`);
 // calculeAge();
 
 
@@ -47,7 +61,7 @@ function calculeAge(){
 }
 
 // nouvelle fonction => arrow function
-const calculeAge2 = ()=>{
+const calculeAge2 = () => {
     const age = year - dob;
 
     console.log(`Voici ton âge : ${age} ans.`); 
@@ -58,8 +72,59 @@ const calculeAge2 = ()=>{
     //     console.log("Dehors mauviette.")
     // }
 
-    age > 18 ? console.log("Rentre bonhomme.") : console.log("Dehors mauviette.") // Condition terniaire
+    age > 18 ? console.log("Rentre bonhomme.") : console.log("Dehors mauviette.") // Condition ternaire
 }
 
 
+async function chargerMusique() {
+    try {
+        const response = await fetch('assets/json/listMusics.json');
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        const tableBody = document.getElementById('listeMusique');
+
+        data.musiques.forEach(musique => {
+            const row = document.createElement('tr');
+            const artisteCell = document.createElement('td');
+            const musiqueCell = document.createElement('td');
+
+            artisteCell.textContent = musique.artiste;
+            musiqueCell.textContent = musique.titre;
+
+            row.appendChild(artisteCell);
+            row.appendChild(musiqueCell);
+            tableBody.appendChild(row);
+        });
+
+        // Ajouter un gestionnaire d'événements pour la recherche
+        document.getElementById('search').addEventListener('input', function () {
+            const searchTerm = this.value.toLowerCase();
+            const rows = tableBody.getElementsByTagName('tr');
+
+            for (let i = 0; i < rows.length; i++) {
+                const artiste = rows[i].getElementsByTagName('td')[0].textContent.toLowerCase();
+                const musique = rows[i].getElementsByTagName('td')[1].textContent.toLowerCase();
+
+                if (artiste.includes(searchTerm) || musique.includes(searchTerm)) {
+                    rows[i].style.display = ''; // Afficher la ligne correspondante
+                } else {
+                    rows[i].style.display = 'none'; // Masquer les lignes qui ne correspondent pas
+                }
+            }
+        });
+
+                // Ajouter une classe visible à la table
+                const table = document.querySelector('.musique-table');
+                table.classList.add("visible");
+    } catch (error) {
+        console.error('Erreur de chargement du fichier JSON', error);
+    }
+}
+
+// Appeler la fonction pour charger la liste de musiques dans le tableau
+chargerMusique();
 calculeAge2();
